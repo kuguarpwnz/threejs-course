e6ee0e5a4be78a489311ff920d0d59ca992546f6
unknown <kuguarpwnz@gmail.com>
2025-10-08 11:42:05 +0300

    first lesson

---
+++ 001-red-cube/script.ts
@@ -0,0 +1,27 @@
+import * as THREE from 'three'
+
+const canvas = document.getElementById('webgl')!
+
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+const scene = new THREE.Scene()
+
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: '#ff0000' })
+const mesh = new THREE.Mesh(geometry, material)
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+
+const renderer = new THREE.WebGLRenderer({
+    canvas,
+})
+renderer.setSize(sizes.width, sizes.height)
+
+scene.add(mesh)
+scene.add(camera)
+
+renderer.render(scene, camera)
