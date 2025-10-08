c0a4a44a05e459841395c85e5aecc284aa24716d
unknown <kuguarpwnz@gmail.com>
2025-10-08 13:00:29 +0300

    finish lesson 4

--- 01-basics/04-transform-objects/src/script.js
+++ 01-basics/04-transform-objects/src/script.js
@@ -12,21 +12,66 @@ const scene = new THREE.Scene()
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
+
+mesh.position.x = 0.7
+mesh.position.y = -0.6
+mesh.position.z = 1
+
+// mesh.scale.x = 2
+// mesh.scale.y = 0.5
+// mesh.scale.z = 0.5
+mesh.scale.set(2, 0.5, 0.5)
+
+mesh.rotation.reorder('YXZ')
+mesh.rotation.y = 0.2 * Math.PI
+mesh.rotation.x = 0.1 * Math.PI
+mesh.rotation.z = 0.1 * Math.PI
+
+const axesHelper = new THREE.AxesHelper(1)
+
+const group = new THREE.Group()
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: '#ccff00' })
+)
+cube1.position.x = 1
+group.add(cube1)
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: '#ff00cc' })
+)
+cube2.position.y = 1
+group.add(cube2)
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: '#00ccff' })
+)
+cube3.position.z = 1
+group.add(cube3)
+group.scale.set(0.3, 0.3, 0.3  )
+group.rotation.z = 0.2
+
+scene.add(group)
 scene.add(mesh)
+scene.add(axesHelper)
 
 /**
  * Sizes
  */
 const sizes = {
     width: 800,
-    height: 600
+    height: 500
 }
 
 /**
  * Camera
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.x = 1
+camera.position.y = 1
 camera.position.z = 3
+camera.lookAt(mesh.position)
+
 scene.add(camera)
 
 /**
